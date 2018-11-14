7a5e89da3e5e85aa1f4bffa72ac301e180c2c3ef
Ingrid Silapan <ingridsilapan@gmail.com>
2018-11-14 15:17:58 +1300

    Added blank Swift project file

---
+++ ios-boilerplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-boilerplate
+//
+//  Created by Ingrid Silapan on 14/11/18.
+//  Copyright © 2018 irs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
